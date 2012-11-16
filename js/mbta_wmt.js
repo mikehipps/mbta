a0a308234fe46e4bf03cf17e28f8c4ca6785f36b
unknown <mikehipps@gmail.com>
2012-11-15 23:52:22 -0500

    dynamic js submit

--- js/mbta_wmt.js
+++ js/mbta_wmt.js
@@ -2,13 +2,21 @@ $(document).ready(function () {
   $("#whichtrain_blue").parent().hide()
   $("#whichtrain_orange").parent().hide();
   $("#whichtrain_red").parent().hide();
+  $(".whichtrain_submit").hide();
+
   
-   $("#mbta-whichline").change(function() {
-      $line = $(this).val();
-      $div = "#whichtrain_" + $line;
-      $("#whichtrain_blue").parent().hide()
-      $("#whichtrain_orange").parent().hide();
-      $("#whichtrain_red").parent().hide();
-      $($div).parent().show();
-  })
+  $("#mbta-whichline").change(function() {
+    $line = $(this).val();
+    $div = "#whichtrain_" + $line;
+    $("#whichtrain_blue").parent().hide()
+    $("#whichtrain_orange").parent().hide();
+    $("#whichtrain_red").parent().hide();
+    $($div).parent().show();
+    $($div).change(function() {
+      $stop = $(this).val();
+      window.location.href = "/mbta/wheresmytrain?stop=" + $stop + '&line=' + $line;  
+    });
+  });
+  
+
 });
